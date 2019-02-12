78230e5122970c88d0c123369762a14f1593a6b2
AvatharN <ssecsd@gmail.com>
2019-02-13 01:12:59 +0300

    Wrong messagestart fix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -373,7 +373,7 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0x5b;
+        pchMessageStart[0] = 0x4b;
         pchMessageStart[1] = 0xac;
         pchMessageStart[2] = 0x2d;
         pchMessageStart[3] = 0xf9;
