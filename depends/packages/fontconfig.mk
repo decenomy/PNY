de6850101d70ef06acb9ed0768de89de11c6aa0f
AvatharN <avathar.noir@gmail.com>
2019-02-19 09:27:33 +0300

    Bump deps versions

--- depends/packages/fontconfig.mk
+++ depends/packages/fontconfig.mk
@@ -1,8 +1,8 @@
 package=fontconfig
-$(package)_version=2.11.1
+$(package)_version=2.11.91
 $(package)_download_path=http://www.freedesktop.org/software/fontconfig/release/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=dc62447533bca844463a3c3fd4083b57c90f18a70506e7a9f4936b5a1e516a99
+$(package)_sha256_hash=7a81e819a74a2848b5b5e9e3f1b1d69fd069844ad1690544d27391959b347bb4
 $(package)_dependencies=freetype expat
 
 define $(package)_set_vars
