c4995b2dec11177d24f04c03f2ab3d7f0f7ff70d
CryptoDev <ronanmcgrory@gmail.com>
2019-04-24 19:06:48 +0000

    Add json files

---
+++ src/invalid_serials.json.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#ifndef PNY_INVALID_SERIALS_JSON_H
+#define PNY_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str = "[\n"
+            "  {\n"
+            "    \"s\": \"c9c868bb56eacfc4f3d829528a0ae812dff26619cd38e6c9a0eea1eacddc84\"\n"
+            "  },\n"
+            "]";
+    return str;
+}
+
+#endif //PNY_INVALID_SERIALS_JSON_H
